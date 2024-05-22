455bd80d1ac44f15e6184b5aae71f6edb7a58940
AndyDevX <andresag253@gmail.com>
2024-05-22 10:56:06 -0600

    Generar campos para inversión

--- public/inversion.php
+++ public/inversion.php
@@ -13,7 +13,7 @@
         <div class="container" id="investment-container">
             <h3>Inversión requerida en activos</h3>
             <div class="table-wrapper">
-                <table id="">
+                <table id="actives-table">
                     <tr>
                         <th>No</th>
                         <th>Descripción</th>
@@ -21,17 +21,11 @@
                         <th>Años de vida útil</th>
                     </tr>
                     
-                    <tr>
-                        <td>1</td>
-                        <td><input type="text" value="Computadora"></td>
-                        <td><input type="number" min="0" value="60000"></td>
-                        <td><input type="number" min="0" value="5"></td>
-                    </tr>
                 </table>
             </div>
 
             <div class="btn-group" id="btns-investment">
-                <button>Nuevo activo</button>
+                <button onclick="newRow()">Nuevo activo</button>
                 <button>Confirmar activos</button>
                 <button>Modificar activos</button>
             </div>
@@ -42,4 +36,54 @@
     </div>
     
 </body>
+
+<script>
+    let num = 0;
+
+    function newRow() {
+        var table =document.getElementById('actives-table');
+        num++;
+
+        let newRow = document.createElement('tr');
+
+        let newNum = document.createElement('td');
+        newNum.innerText = num;
+
+        let newDescription = document.createElement('td');
+            let newInput = document.createElement('input');
+            newInput.setAttribute('type', 'text');
+            newInput.setAttribute('placeholder', 'Descripción del activo');
+
+        let newValue = document.createElement('td');
+            let newInput2 = document.createElement('input');
+            newInput2.setAttribute('type', 'number');
+            newInput2.setAttribute('min', '0');
+            newInput2.setAttribute('placeholder', 'Valor del activo');
+
+        let newUtility = document.createElement('td');
+            let newInput3 = document.createElement('input');
+            newInput3.setAttribute('type', 'number');
+            newInput3.setAttribute('min', '0');
+            newInput3.setAttribute('placeholder', 'Vida útil del activo');
+
+
+    //Anidar elementos
+        //Descripción
+        newDescription.appendChild(newInput);
+
+        //Valor
+        newValue.appendChild(newInput2);
+
+        //Utilidad
+        newUtility.appendChild(newInput3);
+
+        newRow.appendChild(newNum);
+        newRow.appendChild(newDescription);
+        newRow.appendChild(newValue);
+        newRow.appendChild(newUtility);
+
+        table.appendChild(newRow);
+    }
+</script>
+
 </html>
