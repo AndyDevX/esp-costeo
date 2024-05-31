6039526885b67b9200bc0c1e0e62317a474efe78
AndyDevX <andresag253@gmail.com>
2024-05-30 18:37:12 -0600

    Avances calc financ

--- src/php/session_check.php
+++ src/php/session_check.php
@@ -1,7 +1,7 @@
 <?php
-session_start();
+    session_start();
 
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-    header("location: ../../login.html");
-    exit;
-}
+    if (!isset ($_SESSION ['loggedin']) || $_SESSION ['loggedin'] !== true) {
+        header ("location: ../../login.html");
+        exit;
+    }
