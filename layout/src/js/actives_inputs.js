e01b1becf1ceef8095967ec2232ca2ab2922e670
AndyDevX <andresag253@gmail.com>
2024-06-05 00:07:20 -0600

    ARREGLAR SUMATORIA DE ACTIVOS

--- layout/src/js/actives_inputs.js
+++ layout/src/js/actives_inputs.js
@@ -1,4 +1,6 @@
 let activos_number = 0;
+let preoperativos_number = 0;
+let operativos_number = 0;
 
 function RowActives() {
     activos_number++;
@@ -47,6 +49,46 @@ function RowActives() {
     newRow.appendChild(newDescription);
     newRow.appendChild(newValue);
     newRow.appendChild(newLife);
+
+    table.appendChild(newRow);
+}
+
+function RowPreoperatives () {
+    preoperativos_number++;
+
+    // Obtenemos la tabla
+    let table = document.getElementById('preoperatives-table');
+    let newRow = document.createElement('tr');
+
+    // Generamos el número
+    let newNumber = document.createElement('td');
+    newNumber.innerText = preoperativos_number;
+    newNumber.setAttribute('class', 'columna1');
+
+    // Creamos la celda
+    let newDescription = document.createElement('td');
+        // Creamos el input
+        let newInputDescription = document.createElement('input');
+        newInputDescription.setAttribute('type','text');
+        newInputDescription.setAttribute('placeholder', 'Descripción del gasto');
+    newDescription.setAttribute('class', 'columna2');
+
+    // Celda para valor
+    let newValue = document.createElement('td');
+        // Creamos el input
+        let newInputValue = document.createElement('input');
+        newInputValue.setAttribute('type', 'number');
+        newInputValue.setAttribute('placeholder', 'Valor del gasto');
+        newInputValue.setAttribute('min', '0');
+    newValue.setAttribute('class', 'columna3');
+
+    // Anidamos los elementos
+    newDescription.appendChild(newInputDescription);
+    newValue.appendChild(newInputValue);
+
+    newRow.appendChild(newNumber);
+    newRow.appendChild(newDescription);
+    newRow.appendChild(newValue);
 
     table.appendChild(newRow);
 }
--- layout/src/js/step_control.js
+++ layout/src/js/step_control.js
@@ -1,7 +1,23 @@
-var json;
+var costeo_json;
+var inversion_json;
 var name;
 
+var totalActivos = [];
+
+var datos = {
+    "activos": [],
+    "preoperativos": [],
+    "operativos": []
+};
+
+// Primer contenedor de Costeo
 document.getElementById("seccion1").style.display = "block";
+
+// Primer contenedor de Financiamiento
+document.getElementById("seccion5").style.display = "block";
+
+
+
 
 function enableSection(actual, next) {
     document.getElementById(actual).style.display = "none";
@@ -19,16 +35,21 @@ function disableSection(actual, previous) {
 function setName () {
     let name = document.getElementById("prod_name").value;
 
-    document.getElementById("prod_header1").innerText = "Insumos directos aplicados en " + name;
-    document.getElementById("prod_header2").innerText = "Distribucion de costos de insumos de acuerdo a su uso en " + name;
+    if (name === "") {
+        alert("Por favor, ingrese un nombre de producto o servicio");
+    } else {
+        document.getElementById("prod_header1").innerText = "Insumos directos aplicados en " + name;
+        document.getElementById("prod_header2").innerText = "Distribucion de costos de insumos de acuerdo a su uso en " + name;
 
-    // Definir en las columnas de la sección 3
-    this.name = name;
+        enableSection('seccion1', 'seccion2');
+        // Definir en las columnas de la sección 3
+        this.name = name;
+    }
 }
 
 // Sección 2
 function readData() {
-    json = tableToJson();
+    costeo_json = tableToJson("input-table");
     
         var table = document.getElementById('tabla-prorrateo').getElementsByTagName('tbody')[0];
         table.innerHTML = '';
@@ -37,7 +58,7 @@ function readData() {
 
         var rowIndex = 0;
 
-        json.forEach (function(insumo, index) {
+        costeo_json.forEach (function(insumo, index) {
             if (insumo.unit === "Día") {
                 let newObject = {
                     "number": null,
@@ -47,13 +68,13 @@ function readData() {
                     "amount": insumo.description
                 };
 
-                json.push (newObject);
+                costeo_json.push (newObject);
             }
         });
 
-        console.log (json);
+        console.log (costeo_json);
     
-        json.forEach(function(insumo, index) {
+        costeo_json.forEach(function(insumo, index) {
             var row = table.insertRow();
             var numeroCell = row.insertCell(0);
             var descripcionCell = row.insertCell(1);
@@ -69,7 +90,7 @@ function readData() {
             
         });
 
-        console.log(json);
+        console.log(costeo_json);
 }
 
 
@@ -85,11 +106,11 @@ function distribucion() {
     
             if (input) {
                 let valorInput = input.value;
-                json[i].amount = valorInput;  // Ajuste del índice del JSON
+                costeo_json[i].amount = valorInput;  // Ajuste del índice del JSON
             }
         }
     
-        console.log(JSON.stringify(json, null, 2));
+        console.log(JSON.stringify(costeo_json, null, 2));
 
     enableSection("seccion3", "seccion4");
 }
@@ -97,28 +118,10 @@ function distribucion() {
     // Confirmar distribución y mostrar resultado
 function costTable() {
     let table = document.getElementById("tabla-costeo");
-    var data = json;
+    let data = costeo_json;
 
     var costoTotal = 0;
-/*
-    // Limpiar la tabla
-    tableBody.innerHTML = "";
 
-    // Iterar en el JSON
-    data.forEach(item => {
-        // Nueva fila
-        const row = document.createElement("tr");
-
-        // Crear celdas de la fila
-        Object.values(item).forEach(value => {
-            const cell = document.createElement("td");
-            cell.textContent = value;
-            row.appendChild(cell);
-        });
-
-        tableBody.appendChild(row);
-    });
-*/
     while(table.rows.length > 1) {
         table.deleteRow(1);
     }
@@ -169,21 +172,87 @@ function costTable() {
         }
     });
 
-    document.getElementById("totalCost").textContent = costoTotal.toFixed(2);
+    document.getElementById("totalCost").textContent = costoTotal.toLocaleString('en-US');;
 }
 
 
+    // Financiamiento
+// Sección 5
+/*
+function readActives() {
+    // Generamos el JSON
+    inversion_json = tableToJson("actives-table");
 
+    let data = inversion_json;
 
+    if (!Array.isArray(data)) {
+        console.error("Error: El JSON no es un array");
+        return;
+    }
+
+    let totalActivos = data.reduce((sum, item) => sum + parseFloat(item.activeValue), 0);
+    
+    document.getElementById("totalActivos").textContent = totalActivos.toLocaleString('en-US');
+    document.getElementById("totalActivosContainer").style.display = "block";
+
+    enableSection("seccion5", "seccion6");
+}
+*/
+
+function readFinanciamientoTable(idTabla, tipo) {
+    let tabla = document.getElementById(idTabla);
+    let filas = tabla.rows;
+
+    for (let i = 1; i < filas.length; i++) { // Empezamos en 1 para omitir la fila de encabezados
+        let celdas = filas[i].cells;
+        let objeto = {
+            "numero": celdas[0].innerText.trim(),
+            "descripcion": celdas[1].querySelector('input') ? celdas[1].querySelector('input').value.trim() : '',
+            "valor": celdas[2].querySelector('input') ? celdas[2].querySelector('input').value.trim() : '',
+            "años": celdas[3] && celdas[3].querySelector('input') ? celdas[3].querySelector('input').value.trim() : '',
+            "periodicidad": celdas[4] && celdas[4].querySelector('input') ? celdas[4].querySelector('input').value.trim() : '',
+            "valor_anualizado": celdas[5] && celdas[5].querySelector('input') ? celdas[5].querySelector('input').value.trim() : ''
+        };
+
+        // Agregar el objeto al array correspondiente en datos
+        datos[tipo].push(objeto);
+    }
+
+    console.log(JSON.stringify(datos, null, 2));
+    enableSection("seccion5", "seccion6");
+}
+
+// NO FUNCIONA ESTA PORQUERÍA, MAÑANA AVERIGUO CÓMO
+function sumarValoresActivos(datos) {
+    const activos = datos.activos;
+
+    const suma = activos.reduce((total, activo) => {
+        return total + parseInt(activo.valor);
+    }, 0);
+
+    console.log(suma);
+    return suma;
+}
+
+function setTotalActivos(datos) {
+    let p = document.getElementById("totalActivosContainer");
+    let span = document.getElementById("totalActivos");
+    let totalActivos = sumarValoresActivos(datos);
+
+    p.style.display = "block";
+    span.innerText = totalActivos;
+}
+
+// Sección 7
 
 
 
 
 
 // Funciones extra
-function tableToJson() {
+function tableToJson(tabla) {
     // Obtener la tabla
-    var table = document.getElementById('input-table');
+    var table = document.getElementById(tabla);
 
     // Arreglo para guardar los datos
     var data = [];
@@ -198,22 +267,40 @@ function tableToJson() {
             var cell = row.cells[j];
             var header = table.rows[0].cells[j].id;
 
-            // Guardar el valor de la celda en el objeto JSON usando el nombre de la columna como clave
-            if (header === 'description') {
-                // Si es la columna de descripción, obtener el valor del input
-                // console.log(cell.querySelector('input').value);
-                rowData[header] = cell.querySelector('input').value;
-            } else if (header === 'unit') {
-                // Si es la columna de unidad, obtener el valor seleccionado del select
-                var select = cell.querySelector('select');
-                rowData[header] = units[select.selectedIndex];
-            } else if (header === 'price') {
-                // Si es la columna de precio, obtener el valor del input
-                rowData[header] = cell.querySelector('input').value;
-            } else {
-                // Si es cualquier otra columna, obtener el texto directamente
-                rowData[header] = cell.innerText;
+            if (tabla === "input-table") {
+                // Guardar el valor de la celda en el objeto JSON usando el nombre de la columna como clave
+                if (header === 'description') {
+                    // Si es la columna de descripción, obtener el valor del input
+                    // console.log(cell.querySelector('input').value);
+                    rowData[header] = cell.querySelector('input').value;
+                } else if (header === 'unit') {
+                    // Si es la columna de unidad, obtener el valor seleccionado del select
+                    var select = cell.querySelector('select');
+                    rowData[header] = units[select.selectedIndex];
+                } else if (header === 'price') {
+                    // Si es la columna de precio, obtener el valor del input
+                    rowData[header] = cell.querySelector('input').value;
+                } else {
+                    // Si es cualquier otra columna, obtener el texto directamente
+                    rowData[header] = cell.innerText;
+                }
+            } else if (tabla === "actives-table") {
+                
+                if (header === 'activeDescription') {
+                    rowData[header] = cell.querySelector('input').value;
+
+                } else if (header === 'activeValue') {
+                    rowData[header] = cell.querySelector('input').value;
+
+                } else if (header === 'activeYears') {
+                    rowData[header] = cell.querySelector('input').value;
+
+                } else {
+                    rowData[header] = cell.innerText;
+                }
             }
+
+            
         }
 
         // Agregar el campo "amount" vacio al objeto JSON de la fila
